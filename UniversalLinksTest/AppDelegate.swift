23812bb24354576bebcdd192d83856b69a43a853
Jasper Reddin <jasper@jasperreddin.com>
2019-07-24 13:13:24 -0500

    Finish universal links

--- UniversalLinksTest/AppDelegate.swift
+++ UniversalLinksTest/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var eventualData: String?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -41,6 +41,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        
+        self.eventualData = "We got em bois"
+        
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+            let incomingURL = userActivity.webpageURL,
+            let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true),
+            let path = components.path else {
+                return false
+        }
+        
+        print(incomingURL.absoluteString)
+        
+        self.eventualData = incomingURL.absoluteString
+        
+        if let vc = window?.rootViewController as? ViewController {
+            vc.label.text = eventualData
+        }
+        
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        print(url.absoluteString)
+        
+        self.eventualData = "We also got em bois"
+        
+        return true
+    }
 
 }
 
--- UniversalLinksTest/ViewController.swift
+++ UniversalLinksTest/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var eventualData = "I'm a cow"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let delegate = UIApplication.shared.delegate as? AppDelegate,
+            let string = delegate.eventualData{
+            
+            self.eventualData = string
+        } else {
+            self.eventualData = Date(timeIntervalSinceNow: 0).description
+        }
+        
+        self.label.text = eventualData
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 }
 
